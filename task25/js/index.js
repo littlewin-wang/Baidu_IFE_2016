f583d21414925bf4d989999e1578d860752f5d82
littlewin <littlewin.wang@gmail.com>
2016-06-03 22:49:52 +0800

    update

--- task25/js/index.js
+++ task25/js/index.js
@@ -1,1 +1,35 @@
-var treeDemo = newTree("前端工程师");
+// ===============================事件处理=============================
+function addEvent(element, type, handler) {
+  if (element.addEventListener) {
+    element.addEventListener(type, handler);
+  }
+  else if (element.attachEvent) {
+    element.attachEvent("on" + type, handler);
+  }
+  else {
+    element["on" + type] = handler;
+  }
+}
+
+// ===============================新建实例=============================
+var tree = new Tree("前端大全");
+document.getElementById("treeArea").appendChild(tree._root.treeElement)
+tree.add('HTML5', '前端大全', tree.traverseBF);
+tree.add('语义化', 'HTML5', tree.traverseBF);
+tree.add('结构和样式分离', 'HTML5', tree.traverseBF);
+tree.add('CSS3', '前端大全', tree.traverseBF);
+tree.add('JavaScript', '前端大全', tree.traverseBF);
+
+addEvent(tree._root.treeElement, "click", function (e) {
+  var target = e.target || e.srcElement;
+
+  var targetItem = target.parentNode.getElementsByTagName("span")[0].innerHTML;
+  var parentItem = target.parentNode.parentNode.getElementsByTagName("span")[0].innerHTML;
+
+  if (target.className == "add") {
+    tree.add(prompt("请输入子结点的内容："), targetItem, tree.traverseBF);
+  }
+  else if (target.className == "delete") {
+    tree.remove(targetItem, parentItem, tree.traverseBF);
+  }
+});
--- task25/js/node.js
+++ task25/js/node.js
@@ -2,10 +2,35 @@ function Node(data) {
   this.data = data;
   this.parent = null;
   this.children = [];
+
+  var newNode = document.createElement("div");
+  newNode.className = "item"
+  var newArrow = document.createElement("div");
+  newArrow.className = "arrow down";
+  var newTitle = document.createElement("span");
+  newTitle.className = "node-title";
+  newTitle.innerHTML = data;
+  var newTab = document.createElement("span");
+  newTab.className = "Tab";
+  newTab.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;";
+  var newAdd = document.createElement("img");
+  newAdd.className = "add";
+  newAdd.src = "images/add.png";
+  var newRemove = document.createElement("img");
+  newRemove.className = "delete";
+  newRemove.src = "images/remove.png";
+  newNode.appendChild(newArrow);
+  newNode.appendChild(newTitle);
+  newNode.appendChild(newTab);
+  newNode.appendChild(newAdd);
+  newNode.appendChild(newRemove);
+
+  this.treeElement = newNode;
 }
  
 function Tree(data) {
   var node = new Node(data);
+  node.treeElement.removeChild(node.treeElement.lastChild)
   this._root = node;
 }
  
@@ -49,6 +74,7 @@ Tree.prototype.add = function(data, toData, traversal) {
 
   if (parent) {
     parent.children.push(child);
+    parent.treeElement.appendChild(child.treeElement)
     child.parent = parent;
   } else {
     throw new Error('Cannot add node to a non-existent parent.');
@@ -76,6 +102,7 @@ Tree.prototype.remove = function(data, fromData, traversal) {
       throw new Error('Node to remove does not exist.');
     } else {
       childToRemove = parent.children.splice(index, 1);
+      parent.treeElement.removeChild(parent.treeElement.getElementsByClassName("item")[index]);
     }
   } else {
     throw new Error('Parent does not exist.');
