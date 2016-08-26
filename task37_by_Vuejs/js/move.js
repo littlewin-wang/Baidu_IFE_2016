a8e8aa3618ebef8e7af370021475dbb11b31c5ab
littlewin <littlewin.wang@gmail.com>
2016-08-26 10:30:41 +0800

    update task37

---
+++ task37_by_Vuejs/js/move.js
@@ -0,0 +1,121 @@
+(function() {
+  var pop_box = document.getElementById('pop_box');
+
+  //获取屏幕高度
+  var windowSize = {
+    width: document.documentElement.offsetWidth
+        || document.documentElement.clientWidth,
+    height: window.innerHeight || document.body.clientHeight
+  }
+
+  //事件绑定函数，其中eventType为不含"on"的事件类型
+  function bind(ele, eventType, callback) {
+    if(ele.addEventListener) {
+      // W3C标准写法
+      return ele.addEventListener(eventType, callback, false);
+    }else if(ele.attachEvent) {
+      // 兼容IE6~8
+      return ele.attachEvent("on" + eventType, callback);
+    }else {
+      // 兼容IE5-
+      return ele["on" + eventType] = callback;
+    }
+  }
+
+  // 浮动层的移动事件绑定
+  bind(pop_box, "mousedown", popDown);
+  bind(pop_box, "mousemove", popMove);
+  bind(pop_box, "mouseup", popUp);
+
+  // isDown表示鼠标是否已按下，默认为未按下状态
+  var isDown = false;
+  // 鼠标位置与浮动层左上角的距离
+  var deltaX, deltaY;
+  // 浮动层的margin值
+  var marginX, marginY;
+  // 声明浮动层移动的边界值
+  var edgeLeft, edgeTop;
+  // 声明鼠标当前坐标
+  var mouseX, mouseY;
+  // 浮动层元素左上角的真实left和top值
+  var popUpBoxLeft, popUpBoxTop;
+
+  // 浮动层的鼠标按下事件
+  function popDown(e) {
+    // 修改拖动样式
+    pop_box.style.cursor = "move";
+
+    // 获取当前鼠标位置坐标
+    var e = e || window.event;
+    var mouse = getPosition(e);
+    mouseX = mouse[0];
+    mouseY = mouse[1];
+
+    // 获取浮动层元素左上角的真实left和top值
+    popUpBoxLeft = pop_box.offsetLeft - marginX;
+    popUpBoxTop = pop_box.offsetTop - marginY;
+
+    // 获取鼠标位置与浮动层左上角的距离
+    deltaX = mouseX - popUpBoxLeft;
+    deltaY = mouseY - popUpBoxTop;
+
+    // 浮动层的margin值
+    marginX = -pop_box.offsetWidth/2;
+    marginY = -pop_box.offsetHeight/2;
+
+    // 确认鼠标是否按下
+    isDown = true;
+  }
+
+  // 浮动层的鼠标移动事件
+  function popMove(e) {
+    // 获取当前鼠标位置坐标
+    var e = e || window.event;
+    var mouse = getPosition(e);
+    mouseX = mouse[0];
+    mouseY = mouse[1];
+
+    // 当鼠标已经按下，触发鼠标移动事件
+    if(isDown) {
+      // 浮动层移动边界判断
+      edgeLeft = mouseX - deltaX;
+      edgeTop = mouseY - deltaY;
+      if((edgeLeft >= -marginX) && (edgeLeft < windowSize.width - pop_box.offsetWidth - marginX)) {
+        pop_box.style.left = edgeLeft + "px";
+      }
+      if((edgeTop >= -marginY) && (edgeTop < windowSize.height - pop_box.offsetTop - marginY)){
+        pop_box.style.top = edgeTop + "px";
+      }
+    }
+  }
+
+  // 浮动层的鼠标释放事件
+  function popUp(e) {
+    pop_box.style.cursor = "default";
+    isDown = false;
+  }
+
+  // 获取鼠标当前位置
+  function getPosition(e) {
+    // 访问事件对象
+    var e = e || window.event;
+
+    // 声明x、y分别为鼠标相对于文档的位置
+    var x = 0, y = 0;
+
+    // 针对W3C标准浏览器具有pageX/Y属性
+    if(e.pageX) {
+      x = e.pageX;
+      y = e.pageY;
+    }else if(e.clientX) {
+      // 在IE6~8中不支持pageX，而是用clientX；但clientX只是相对屏幕视口的横坐标，不包含滚动宽度；
+      var scrollLeft = document.documentElement.scrollLeft || document.body.scrollLeft;
+      var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+      // pageX = clientX + scrollX
+      x = e.clientX + scrollLeft;
+      y = e.clientY + scrollTop;
+    }
+    return [x, y];
+  }
+
+})()
